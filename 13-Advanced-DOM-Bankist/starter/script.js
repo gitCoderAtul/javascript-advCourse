5aa3506cec101087ab69c20aca167de8ac55630b
unknown <atulk9996@gmail.com>
2022-01-30 10:04:11 +0530

    Dom trasnversing and tabs

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -30,6 +30,35 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+//page navigation
+// document.querySelectorAll('.nav__link').forEach(element => {
+//   element.addEventListener('click', function(e){
+//     e.preventDefault();
+//     console.log('link');
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({
+//       behavior:'smooth'
+//     });
+    
+//   })
+// });
+
+//determine what element originated the event
+document.querySelector('.nav__links').addEventListener('click',function(e){
+console.log(e.target);
+e.preventDefault();
+
+if(e.target.classList.contains('nav__link')){
+  console.log('link');
+  const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({
+      behavior:'smooth'
+    });
+}
+
+});
 
 // selecting
 
@@ -127,13 +156,122 @@ console.log('current scroll x/y ' + window.pageXOffset, pageYOffset);
 // s1coords.top + window.pageYOffset
 // );
 
+//old way
 window.scrollTo({
   left: s1coords.left + window.pageXOffset,
   top : s1coords.top + window.pageYOffset,
   behavior: 'smooth' 
 });
 
+//modern - for latest browser only
 section1.scrollIntoView({behavior:'smooth'});
-
-
 });
+
+//event
+const h1 = document.querySelector('h1');    
+const alertH1 = function(e){
+  alert('Great! You are reading the heading :D');
+}
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 300);
+
+// Great! You are reading the heading :D'
+// h1.addEventListener('mouseenter', function(e){
+//   alert('Great! You are reading the heading :D');
+// })
+
+//event propagation practice
+const randomInt = (min, max) => 
+Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () => 
+`rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`;
+//console.log(randomInt(0, 255));
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  //console.log('LINK', e.target, e.currentTarget);
+  //console.log(e.currentTarget === this);
+
+  // Stop propagation
+ // e.stopPropagation();
+});
+
+// document.querySelector('.nav__links').addEventListener('click', function(e){
+//    this.style.backgroundColor = randomColor();
+// });
+
+// document.querySelector('.nav').addEventListener('click', function(e){
+//    this.style.backgroundColor = randomColor();
+// });
+
+//Dom Transverse
+//  //Going upwards: parents
+//  .childNodes  .children 
+//  .firstElementcChild  .lastElementChild
+//  //going upwards: parents
+//  .parentNode  .parentElement
+//  .closest  
+//  //Going sidewats: siblings
+//.previousElementSibling   .nextElementsSibling
+//  .previousSibling  .nextSibling
+
+const he = document.querySelector('h1');
+console.log(he.childNodes);
+console.log(he.children);
+
+console.log(he.parentNode);
+console.log(he.parentElement);
+
+he.closest('header').style.background = 'var(--gradient-secondary)';
+he.closest('h1').style.background = 'var(--gradient-primary)';
+
+console.log(he.previousElementSibling);
+console.log(he.nextElementSibling);
+
+console.log(he.previousSibling);
+console.log(he.nextSibling);
+
+console.log(he.parentElement.children);
+[...he.parentElement.children].forEach(function(el){
+  if(el !== he)
+    el.style.transform ='scale(0.5)';
+  
+});
+
+
+//tab component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer =document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function(e){
+  const clicks = e.target.closest('.operations__tab');
+  console.log(clicks);
+
+  //old way guard close
+  // if(clicks){
+  //   //will not get error if we write this type of code
+  //   clicks.classList.add('operations__tab--active');
+  // } 
+  //modern way - guard close
+
+  if(!clicks) return;
+  
+  //Remove active tab
+  tabs.forEach((t) => {t.classList.remove('operations__tab--active')
+});
+tabsContent.forEach((c) => {c.classList.remove('operations__content--active')});
+
+  //Active tab
+  clicks.classList.add('operations__tab--active');
+
+  //Active content tab
+  console.log(clicks.dataset.tab);
+  document.querySelector(`.operations__content--${clicks.dataset.tab}`)
+  .classList.add('operations__content--active')
+})
+
+
