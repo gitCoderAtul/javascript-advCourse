731d48aa76d8dadb6bfa305193fd26234a5c7785
unknown <atulk9996@gmail.com>
2022-03-03 20:06:20 +0530

    Pig Game - Roll Dice

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,42 @@
 'use strict';
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+//starting condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden'); 
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function(){
+    // Dice number
+    const dice = Math.trunc(Math.random() * 6 + 1);
+
+    console.log( dice + ' Roll button');
+    //display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    //check the rolled 1
+    if(dice !== 1){
+        //Add dice to current score
+        currentScore = currentScore + dice;
+        current0El.textContent = currentScore;
+    }else{
+        //switch next player
+    }
+
+});
+btnNew.addEventListener('click', function(){
+    console.log('New button');
+});
+btnHold.addEventListener('click', function(){
+    console.log('Hold button');
+});
